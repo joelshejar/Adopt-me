e8eb097174338044efcd43c820cdd288e4b6c145
joelshejar <joel.rajesh13@gmail.com>
2023-02-20 19:01:27 +0530

    Setup form

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
