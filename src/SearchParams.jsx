b0c2c723cac5343ba006e0016e5c7eafe3773621
joelshejar <joel.rajesh13@gmail.com>
2023-02-20 22:11:33 +0530

    Implemented useBreedList

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
 const SearchParams = () => {
@@ -7,7 +8,7 @@ const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = [];
+  const [breeds] = useBreedList(animal);
 
   useEffect(() => {
     requestPets();
