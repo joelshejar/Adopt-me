4b6e1450e96ac0298398991f46d69217d8a045d4
joelshejar <joel.rajesh13@gmail.com>
2023-02-21 08:25:28 +0530

    Implemented Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { createRoot } from "react-dom/client";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
