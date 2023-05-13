5255bbd336364201d1a45593f3e8406075da7231
joelshejar <joel.rajesh13@gmail.com>
2023-05-13 17:28:08 +0530

    Changed http to https

--- src/fetchBreedList.js
+++ src/fetchBreedList.js
@@ -1,7 +1,7 @@
 const fetchBreedList = async ({ queryKey }) => {
   const animal = queryKey[1];
   const apiRes = await fetch(
-    `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+    `https://pets-v2.dev-apis.com/breeds?animal=${animal}`
   );
 
   if (!apiRes.ok) {
