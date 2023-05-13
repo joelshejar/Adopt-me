62232b45d72013252f4915e3645a5fe790bc39a9
joelshejar <joel.rajesh13@gmail.com>
2023-05-13 17:18:39 +0530

    Fixed an import error

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
-import adoptedPetContext from "./adoptedPetContext";
+import adoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
