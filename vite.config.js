56cee95fed8e0368f2aecd56b36a28cc23aa5aca
joelshejar <joel.rajesh13@gmail.com>
2023-05-17 19:08:52 +0530

    Updated viteconfig

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: "src",
-  // build: {
-  //   outDir: "../dist",
-  // }
+  build: {
+    outDir: "../dist",
+  }
 });
